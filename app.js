01be80d79fc432dbe748989364554df69c95cc30
Robert Warren <2110981@wholefoods.com>
2021-11-15 07:40:30 -0600

    add inquirer andnpm modules

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+//fs.writeFile('index.html', generatePage(name, github), err => {
+  //if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output shit!');
-});
+  
+  //console.log('Portfolio complete! Check out index.html to see the output shit!');
+//});
