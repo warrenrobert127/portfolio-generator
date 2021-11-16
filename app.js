e55ceec57ed1b874a0580c79b74601f5c7fab366
Robert Warren <2110981@wholefoods.com>
2021-11-16 08:16:19 -0600

    add header to index file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -140,16 +142,12 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
   });
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-//fs.writeFile('index.html', generatePage(name, github), err => {
-  //if (err) throw err;
-
-  
-  //console.log('Portfolio complete! Check out index.html to see the output shit!');
-//});
+});
